2d0f8dd4009fb738e4e6c6102bce2702edea8c73
dezirich <98296857+dezirich@users.noreply.github.com>
2022-08-21 15:54:45 -0500

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -22,15 +22,9 @@
         	<input type="email" name="email" id="email" value="<?php echo $_REQUEST['email'] ?>" disabled><br>
         	<label for="verify">Verify Email:</label>
         	<input type="email" name="verify" id="verify" value="<?php echo $_REQUEST['email'] ?>" disabled><br>
-			<label for="phone">Phone Number:</label>
-			<input type="tel" name="phone" id="phone" value="<?php echo $_REQUEST['phone'] ?>" disabled><br>
 		</fieldset>
 		<fieldset>
     		<legend>Message Information</legend>
-			<label for="reservation_date">Today's  Date:</label>
-			<input type="date" name="reservation_date" id="reservation_date" value="<?php echo $_REQUEST['reservation_date'] ?>" disabled><br>
-			<label for="subject">Subject:</label>
-			<input type="text" name="subject" id="subject" value="<?php echo $_REQUEST['subject'] ?>" disabled><br>
 			<label for="Message">Message:</label>
 			<textarea id="message" name="message" rows="4" disabled><?php echo $_REQUEST['message'] ?></textarea>
 		</fieldset>
@@ -44,8 +38,6 @@
 
   //Set PHP variable equal to information completed on the HTML form
     $email = $_REQUEST['email']; //Request email that user typed on HTML form
-    $reservation_date = $_REQUEST['reservation_date']; //Request subject that user typed on HTML form
-    $subject = $_REQUEST['subject']; //Request subject that user typed on HTML form
     $message = $_REQUEST['message']; //Request message that user typed on HTML form
   //Combine first name and last name, adding a space in between
     $name = $_REQUEST['first_name'] . " " .  $_REQUEST['last_name']; 
